653eb083bcb1fa1a0bedaa5088a581c2446d66d2
Tom C <tom.carlier@epitech.eu>
2024-02-24 18:07:42 +0100

    Merge : fusion branch called "case2" with the main branch --> function that do the case 1
    [feat] function that do the case 1 and print it

--- src/case1.py
+++
@@ -1,7 +0,0 @@
-from src.calc_gens import calc_gens
-from src.print_gens import print_gens
-
-
-def do_case1(n: float, k: float) -> int:
-    print_gens([i for i in range(1, 101)], calc_gens(n, k, 1, 100))
-    return 0
