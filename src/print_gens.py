4f600c0e5040e71172fa0b6ef5aa66d534f95745
Tom C <tom.carlier@epitech.eu>
2024-02-24 18:08:05 +0100

    [fix] remove useless character

--- src/print_gens.py
+++ src/print_gens.py
@@ -5,6 +5,6 @@ def print_gen(x: float, y: float, is_int: bool) -> None:
         print("{:.2f} {:.2f}".format(x, y))
 
 
-def print_gens(xs: list[float], ys: list[fl²oat], is_int: bool) -> None:
+def print_gens(xs: list[float], ys: list[float], is_int: bool) -> None:
     for x, y in zip(xs, ys):
         print_gen(x, y, is_int)
