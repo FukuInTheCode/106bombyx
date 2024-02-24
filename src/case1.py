142e11820d4b41acb6c93e5090f0fcdcf25c3fd7
Tom C <tom.carlier@epitech.eu>
2024-02-24 18:07:34 +0100

    [feat] function that calc and print the first case

---
+++ src/case1.py
@@ -0,0 +1,12 @@
+from src.calc_gens import calc_gens
+from src.print_gens import print_gens
+
+
+def tom() -> int:
+    print("je t'aime Tom")
+    return 0
+
+
+def do_case1(n: int, k: int) -> int:
+    print_gens([i for i in range(1, 101)], calc_gens(n, k, 1, 100))
+    return 0
