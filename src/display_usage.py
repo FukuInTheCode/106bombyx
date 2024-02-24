114e474901096545c28f807074f5bdb3b754b90f
Tom C <tom.carlier@epitech.eu>
2024-02-24 18:07:27 +0100

    [feat] function that print a x en y on a line

---
+++ src/display_usage.py
@@ -0,0 +1,9 @@
+def display_usage() -> int:
+    print("USAGE")
+    print("\t./106bombyx n [k | i0 i1]\n")
+    print("DESCRIPTION")
+    print("\tn \tnumber of first generation individuals")
+    print("\tk \tgrowth rate from 1 to 4")
+    print("\ti0 \tinitial generation (included)")
+    print("\ti1 \tfinal generation (included")
+    return 0
---
+++ src/print_gens.py
@@ -0,0 +1,2 @@
+def print_gen(x: int, y: int) -> None:
+    print(x, y)
