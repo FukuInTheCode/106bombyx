86774cdd3f343b83a12c4dd9d60f79ed53410b75
Tom C <tom.carlier@epitech.eu>
2024-02-24 18:07:50 +0100

    [feat] -->  add

--- src/check_arg.py
+++ src/check_arg.py
@@ -1,7 +1,43 @@
+import sys
+
+
+#from src.do_case1 import do_case1.py
+#from src.do_case2 import do_case2.py
+
+
+def is_between_one_and_four(number):
+    if 1 <= number <= 4:
+        return True
+    else:
+        return False
+
+
+def is_int(num):
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
+
+
+def is_float(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+
 def check_arg(abrs: list[str]) -> int:
-    print(abrs)
     if len(abrs) == 2:
-        return 0
+        if is_float(abrs[0]) and is_float(abrs[1]) and float(abrs[0]) > 0 and is_between_one_and_four(float(abrs[1])):
+            #do_case1()
+            return 0
+        sys.exit(84)
     if len(abrs) == 3:
-        return 0
+        if is_float(abrs[0] and is_int(abrs[1]) and is_int(abrs[2])) and int(abrs[0]) > 0 and int(
+                abrs[1]) > 0 and int(abrs[2]) > 0 and int(abrs[2]) > int(abrs[1]) > 0:
+            #do_case2()
+            return 0
+        return 84
     return 84
