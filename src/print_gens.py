393a9ecc68a1223b3091258e0509e8f28de5d327
Tom C <tom.carlier@epitech.eu>
2024-02-24 18:08:02 +0100

    [refactor] add the arguments to difereniate if x is a int or not

--- src/print_gens.py
+++ src/print_gens.py
@@ -1,7 +1,10 @@
-def print_gen(x: float, y: float) -> None:
-    print("{:d} {:.2f}".format(x, y))
+def print_gen(x: float, y: float, is_int: bool) -> None:
+    if is_int:
+        print("{:d} {:.2f}".format(x, y))
+    else:
+        print("{:.2f} {:.2f}".format(x, y))
 
 
-def print_gens(xs: list[float], ys: list[float]) -> None:
+def print_gens(xs: list[float], ys: list[fl²oat], is_int: bool) -> None:
     for x, y in zip(xs, ys):
-        print_gen(x, y)
+        print_gen(x, y, is_int)
